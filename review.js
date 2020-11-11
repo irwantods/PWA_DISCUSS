730a88f94bd0903528376bd3f82066ac558fc7c0
irwanto dwi subagus <66657551+irwantods@users.noreply.github.com>
2020-11-11 16:28:03 +0700

    Add files via upload

--- review.js
+++ review.js
@@ -1,49 +1,27 @@
-function getArticleById() {
-    // Ambil nilai query parameter (?id=)
-    var urlParams = new URLSearchParams(window.location.search);
-    var idParam = urlParams.get("id");
+function showTeam(data) {
+    // let teams = "";
+    let teamElement = document.getElementById("homeTeams");
 
-    if ("caches" in window) {
-        caches.match(base_url + "article/" + idParam).then(function(response) {
-            if (response) {
-                response.json().then(function(data) {
-                    var articleHTML = `
-            <div class="card">
-                <div class="card-image waves-effect waves-block waves-light">
-                  <img src="${data.result.cover}" />
+    data.teams.forEach((team) => {
+        teamElement.innerHTML += `
+    <div class="col s12 m6 team-card">
+        <div class="card">
+        <a href="./pages/detail-team.html?id=${team.id}">
+            <div class="card-image team-img waves-effect waves-block waves-light">
+                <img src="${team.crestUrl.replace(/^http:\/\//i, 'https://')}" class="responsive-img"/>
+            </div> 
+            </a>
+                <div class="card-content center-align">
+                    <h6 class="black-text">${team.name}</h6>
+                    <p>${team.founded}</p>
+                    <p>${team.clubColors}</p>
+                    <p>${team.venue}</p>
+                    <p>${team.address}</p>
+                    <p>${team.phone}</p>
+                    <a href="${team.website}">${team.website}</a>
                 </div>
-                <div class="card-content">
-                  <span class="card-title">${data.result.post_title}</span>
-                  ${snarkdown(data.result.post_content)}
-                </div>
-              </div>
-            `;
-                    // Sisipkan komponen card ke dalam elemen dengan id #content
-                    document.getElementById("body-content").innerHTML = articleHTML;
-                });
-            }
-        });
-    }
-
-    fetch(base_url + "article/" + idParam)
-        .then(status)
-        .then(json)
-        .then(function(data) {
-            // Objek JavaScript dari response.json() masuk lewat variabel data.
-            console.log(data);
-            // Menyusun komponen card artikel secara dinamis
-            var articleHTML = `
-            <div class="card">
-              <div class="card-image waves-effect waves-block waves-light">
-                <img src="${data.result.cover}" />
-              </div>
-              <div class="card-content">
-                <span class="card-title">${data.result.post_title}</span>
-                ${snarkdown(data.result.post_content)}
-              </div>
             </div>
-          `;
-            // Sisipkan komponen card ke dalam elemen dengan id #content
-            document.getElementById("body-content").innerHTML = articleHTML;
-        });
+    </div>
+    `;
+    });
 }
--- review2.js
+++ review2.js
@@ -1,55 +1,32 @@
-function getArticleById() {
-    return new Promise(function(resolve, reject) {
-        // Ambil nilai query parameter (?id=)
-        var urlParams = new URLSearchParams(window.location.search);
-        var idParam = urlParams.get("id");
-        // memuat data dari cache terlebih dahulu sebelum melakukan request ke server api
-        if ("caches" in window) {
-            caches.match(base_url + "article/" + idParam).then(function(response) {
-                if (response) {
-                    response.json().then(function(data) {
-                        var articleHTML = `
-          <div class="card">
-            <div class="card-image waves-effect waves-block waves-light">
-              <img src="${data.result.cover}" />
-            </div>
-            <div class="card-content">
-              <span class="card-title">${data.result.post_title}</span>
-              ${snarkdown(data.result.post_content)}
-            </div>
-          </div>
-        `;
-                        // Sisipkan komponen card ke dalam elemen dengan id #content
-                        document.getElementById("body-content").innerHTML = articleHTML;
-                        // Kirim objek data hasil parsing json agar bisa disimpan ke indexed db
-                        resolve(data);
-                    });
-                }
-            });
-        }
-
-        fetch(base_url + "article/" + idParam)
-            .then(status)
-            .then(json)
-            .then(function(data) {
-                // Objek JavaScript dari response.json() masuk lewat variabel data.
-                console.log(data);
-                // Menyusun komponen card artikel secara dinamis
-                var articleHTML = `
-        <div class="card">
-          <div class="card-image waves-effect waves-block waves-light">
-            <img src="${data.result.cover}" />
-          </div>
-          <div class="card-content">
-            <span class="card-title">${data.result.post_title}</span>
-            ${snarkdown(data.result.post_content)}
-          </div>
-        </div>
-      `;
-                // Sisipkan komponen card ke dalam elemen dengan id #content
-                document.getElementById("body-content").innerHTML = articleHTML;
-                // Kirim objek data hasil parsing json agar bisa disimpan ke indexed db
-                resolve(data);
-            });
+function getSavedTeams() {
+    getAll().then(function(teams) {
+        console.log(teams);
+        // Menyusun komponen card artikel secara dinamis
+        var teamsHTML = "";
+        teams.forEach(function(team) {
+            // var description = team.post_content.substring(0, 100);
+            teamsHTML. += `
+          <div class="col s12 m6 team-card">
+              <div class="card">
+              <a href="./pages/detail-team.html?id=${team.id}&saved=true">
+                  <div class="card-image team-img waves-effect waves-block waves-light">
+                      <img src="${team.crestUrl.replace(/^http:\/\//i, 'https://')}" class="responsive-img"/>
+                  </div> 
+                  </a>
+                      <div class="card-content center-align">
+                          <h6 class="black-text">${team.name}</h6>
+                          <p>${team.founded}</p>
+                          <p>${team.clubColors}</p>
+                          <p>${team.venue}</p>
+                          <p>${team.address}</p>
+                          <p>${team.phone}</p>
+                          <a href="${team.website}">${team.website}</a>
+                      </div>
+                  </div>
+          </div>            
+          `;
+        });
+        // Sisipkan komponen card ke dalam elemen dengan id #body-content
+        document.getElementById("body-content").innerHTML = teamsHTML;
     });
 }
